4652cb6227d92099e75b5e3ee0e4273002591c7c
Alan Palazzolo <alan.palazzolo@startribune.com>
2017-06-29 12:15:42 -0500

    Adding abaility to look for repo field.

--- common/lib/package.js
+++ common/lib/package.js
@@ -5,11 +5,21 @@
 
 // Dependencies
 const _ = require('lodash');
+const path = require('path');
+const fs = require('fs');
+const gitConfig = require('git-config');
 
 // Package building function
 module.exports = function(context, dependencies) {
   context = context || {};
   dependencies = dependencies || {};
+
+  // Attempt to get git info
+  let gitPath = path.join(process.cwd(), '.git/config');
+  let gitInfo = {};
+  if (fs.existsSync(gitPath)) {
+    gitInfo = gitConfig.sync(gitPath);
+  }
 
   return {
     name: _.kebabCase(context.name),
@@ -19,6 +29,10 @@ module.exports = function(context, dependencies) {
       name: context.authorName,
       email: context.authorEmail
     },
+    repository: gitInfo['remote "origin"'] ? {
+      type: 'git',
+      url: gitInfo['remote "origin"'].url
+    } : undefined,
     main: context.main || 'app/index.js',
     scripts: {
       test: 'jest'
