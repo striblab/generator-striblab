15b1c66033fa5f7e716c1ff52b382d7e0cabac1a
Alan Palazzolo <alan.palazzolo@startribune.com>
2017-10-20 11:59:10 -0500

    Adding cache headers for publishing.

--- app/templates/lib/gulp-publish.js
+++ app/templates/lib/gulp-publish.js
@@ -10,6 +10,7 @@ const path = require('path');
 const gutil = require('gulp-util');
 const jeditor = require('gulp-json-editor');
 const rename = require('gulp-rename');
+const moment = require('moment');
 const awspublish = require('gulp-awspublish');
 const AWS = require('aws-sdk');
 const openurl = require('openurl');
@@ -18,7 +19,7 @@ require('dotenv').load({ silent: true });
 
 // Create a publish token in config.json
 function createToken() {
-  return (done) => {
+  return done => {
     let config = getConfig();
 
     if (config.publishToken) {
@@ -27,26 +28,42 @@ function createToken() {
     }
     else {
       config.publishToken = crypto.randomBytes(16).toString('hex');
-      return fs.writeFile(path.join(__dirname, '..', 'config.json'), JSON.stringify(config, null, '  '), done);
+      return fs.writeFile(
+        path.join(__dirname, '..', 'config.json'),
+        JSON.stringify(config, null, '  '),
+        done
+      );
     }
   };
 }
 
 // Write build version of config which is just the token.
 function buildConfig(gulp) {
-  return (done) => {
+  return done => {
     let configPath = getConfigPath();
     if (!configPath) {
-      done(new gutil.PluginError('publish:build', 'Unable to find config.json or config.custom.json.'));
+      done(
+        new gutil.PluginError(
+          'publish:build',
+          'Unable to find config.json or config.custom.json.'
+        )
+      );
     }
 
-    return gulp.src(configPath)
-      .pipe(jeditor(function(config) {
-        return config.publishToken ? { publishToken: config.publishToken } : {};
-      }))
-      .pipe(rename(function(path) {
-        path.basename = path.basename.replace('.custom', '');
-      }))
+    return gulp
+      .src(configPath)
+      .pipe(
+        jeditor(function(config) {
+          return config.publishToken
+            ? { publishToken: config.publishToken }
+            : {};
+        })
+      )
+      .pipe(
+        rename(function(path) {
+          path.basename = path.basename.replace('.custom', '');
+        })
+      )
       .pipe(gulp.dest('build'));
   };
 }
@@ -61,13 +78,23 @@ function publish(gulp) {
       }
     });
     // Public-read by default
-    let headers = {};
+    let headers = {
+      CacheControl:
+        'public, max-age=' + (p.cacheSeconds || 60) + ', must-revalidate',
+      Expires: moment()
+        .add(p.cacheSeconds || 60, 'seconds')
+        .toDate()
+    };
 
     gutil.log('Using bucket "' + p.bucket + '"');
-    return gulp.src('build/**/*')
-      .pipe(rename((path) => {
-        path.dirname = (p.path.substr(-1) === '/' ? p.path : p.path + '/') + path.dirname;
-      }))
+    return gulp
+      .src('build/**/*')
+      .pipe(
+        rename(path => {
+          path.dirname =
+            (p.path.substr(-1) === '/' ? p.path : p.path + '/') + path.dirname;
+        })
+      )
       .pipe(awspublish.gzip())
       .pipe(publisher.publish(headers))
       .pipe(publisher.cache())
@@ -79,14 +106,20 @@ function publish(gulp) {
 function confirmToken() {
   let s3 = new AWS.S3();
 
-  return (done) => {
+  return done => {
     let p = getPublishConfig();
     let config = getConfig();
-    let key = (p.path.substr(-1) === '/' ? p.path : p.path + '/') + 'config.json';
+    let key =
+      (p.path.substr(-1) === '/' ? p.path : p.path + '/') + 'config.json';
 
     // Make sure we have something to compare
     if (!config.publishToken) {
-      return done(new gutil.PluginError('publish:check', 'Unable to find publishToken in config.json. Run "gulp publish:token" to create a token.'));
+      return done(
+        new gutil.PluginError(
+          'publish:check',
+          'Unable to find publishToken in config.json. Run "gulp publish:token" to create a token.'
+        )
+      );
     }
 
     // If force
@@ -96,40 +129,60 @@ function confirmToken() {
     }
 
     // Get remote file
-    s3.getObject({
-      Bucket: p.bucket,
-      Key: key
-    }, (error, data) => {
-      if (error && error.code === 'NoSuchKey') {
-        gutil.log(gutil.colors.yellow('Unable to find "' + key + '" on bucket "' + p.bucket + '".'));
-        return done();
-      }
-      else if (error) {
-        return done(new gutil.PluginError('publish:check', error, { showStack: true }));
-      }
+    s3.getObject(
+      {
+        Bucket: p.bucket,
+        Key: key
+      },
+      (error, data) => {
+        if (error && error.code === 'NoSuchKey') {
+          gutil.log(
+            gutil.colors.yellow(
+              'Unable to find "' + key + '" on bucket "' + p.bucket + '".'
+            )
+          );
+          return done();
+        }
+        else if (error) {
+          return done(
+            new gutil.PluginError('publish:check', error, { showStack: true })
+          );
+        }
 
-      let c;
-      try {
-        c = JSON.parse(zlib.gunzipSync(data.Body).toString('utf-8'));
-      }
-      catch (e) {
-        return done(new gutil.PluginError('publish:check', e, { showStack: true }));
-      }
+        let c;
+        try {
+          c = JSON.parse(zlib.gunzipSync(data.Body).toString('utf-8'));
+        }
+        catch (e) {
+          return done(
+            new gutil.PluginError('publish:check', e, { showStack: true })
+          );
+        }
 
-      if (c && c.publishToken && c.publishToken === config.publishToken) {
-        gutil.log('Tokens match.');
-        return done();
+        if (c && c.publishToken && c.publishToken === config.publishToken) {
+          gutil.log('Tokens match.');
+          return done();
+        }
+        else {
+          return done(
+            new gutil.PluginError(
+              'publish:check',
+              'Tokens do not match, please make sure that this project should be deployed to "' +
+                key +
+                '" on bucket "' +
+                p.bucket +
+                '".  Or, use the --force flag to force the publish.'
+            )
+          );
+        }
       }
-      else {
-        return done(new gutil.PluginError('publish:check', 'Tokens do not match, please make sure that this project should be deployed to "' + key + '" on bucket "' + p.bucket + '".  Or, use the --force flag to force the publish.'));
-      }
-    });
+    );
   };
 }
 
 // Open URL
 function openURL() {
-  return (done) => {
+  return done => {
     let p = getPublishConfig();
     openurl.open(p.url);
     done();
@@ -139,10 +192,13 @@ function openURL() {
 // Get publish config
 function getPublishConfig() {
   let config = getConfig();
-  return argv.production && config.publish.production ? config.publish.production :
-    argv.staging && config.publish.staging ? config.publish.staging :
-      argv.testing && config.publish.testing ? config.publish.testing :
-        config.publish.default;
+  return argv.production && config.publish.production
+    ? config.publish.production
+    : argv.staging && config.publish.staging
+      ? config.publish.staging
+      : argv.testing && config.publish.testing
+        ? config.publish.testing
+        : config.publish.default;
 }
 
 // Get config path
