f6ff35efd53fbb9c67e3cb937d75ae2adb43568a
Alan Palazzolo <alan.palazzolo@startribune.com>
2019-02-04 10:39:25 -0600

    Updating comment and boilerplate code for svelte templates on the client.

--- generators/app/templates/app/index.js
+++ generators/app/templates/app/index.js
@@ -2,10 +2,12 @@
  * Main JS file for project.
  */
 
-// Define globals that are added through the js.globals in
-// the config.json file, here, mostly so linting won't get triggered
-// and its a good queue of what is available:
-// /* global _ */
+/**
+ * Define globals that are added through the js.globals in
+ * the config.json file, here, mostly so linting won't get triggered
+ * and its a good queue of what is available:
+ */
+// /* global $, _ */
 
 // Dependencies
 import utils from './shared/utils.js';
@@ -19,38 +21,87 @@ utils.environmentNoting();
 utils.autoEnablePym();
 <% } %>
 
-// Adding dependencies
-// ---------------------------------
-// Import local ES6 or CommonJS modules like this:
-// import utilsFn from './shared/utils.js';
-//
-// Or import libraries installed with npm like this:
-// import module from 'module';
+/**
+ * Adding dependencies
+ * ---------------------------------
+ * Import local ES6 or CommonJS modules like this:
+ * import utilsFn from './shared/utils.js';
+ *
+ * Or import libraries installed with npm like this:
+ * import module from 'module';
+ */
 
-// Adding Svelte templates in the client
-// ---------------------------------
-// We can bring in the same Svelte templates that we use
-// to render the HTML into the client for interactivity.  The key
-// part is that we need to have similar data.
+
+/**
+ * Adding Svelte templates in the client
+ * ---------------------------------
+ * We can bring in the same Svelte templates that we use
+ * to render the HTML into the client for interactivity.  The key
+ * part is that we need to have similar data.
+ *
+ * First, import the template.  This is the main one, and will
+ * include any other templates used in the project.
+ *
+ *   `import Content from '../templates/_index-content.svelte.html';`
+ *
+ * Get the data parts that are needed.  There are two ways to do this.
+ * If you are using the buildData function to get data, then add make
+ * sure the config for your data has a `local: "content.json"` property
+ *
+ *  1. For smaller datasets, just import them like other files.
+ *     `import content from '../assets/data/content.json';`
+ *  2. For larger data points, utilize window.fetch.
+ *     `let content = await (await window.fetch('../assets/data/content.json')).json();`
+ *
+ * Once you have your data, use it like a Svelte component:
+ *
+ * const app = new Content({
+ *  target: document.querySelector('.article-lcd-body-content'),
+ *  hydrate: true,
+ *  data: {
+ *    content
+ *  }
+ * });
+ */
+
+
+<% if (answers.projectType === 'cms') { %>
+// Common code to get svelte template loaded on the client and hack-ishly
+// handle sharing
 //
-// First, import the template.  This is the main one, and will
-// include any other templates used in the project.
-// import Content from '../templates/_index-content.svelte.html';
+// import Content from '../templates/_index-content.svelte.html
 //
-// Get the data parts that are needed.  There are two ways to do this.
-// If you are using the buildData function to get data, then ?
+// $(document).ready(() => {
+//   // Hack to get share back
+//   let $share = $('.share-placeholder').size()
+//     ? $('.share-placeholder')
+//       .children()
+//       .detach()
+//     : undefined;
+//   let attachShare = !$share
+//     ? undefined
+//     : () => {
+//       $('.share-placeholder').append($share);
+//     };
+
+//   // Main component
+//   const app = new Content({
+//     target: document.querySelector('.article-lcd-body-content'),
+//     hydrate: true,
+//     data: {
+//       attachShare
+//     }
+//   });
+// });
+<% } else { %>
+// Common code to get svelte template loaded on the client.  Probably need data.
 //
-// 1. For smaller datasets, just import them like other files.
-// import content from '../assets/data/content.json';
-//
-// 2. For larger data points, utilize window.fetch.
-// let content = await (await window.fetch('../assets/data/content.json')).json();
-//
-// Once you have your data, use it like a Svelte component:
+// import Content from '../templates/_index-content.svelte.html
 //
 // const app = new Content({
 //   target: document.querySelector('.article-lcd-body-content'),
+//   hydrate: true,
 //   data: {
-//     content
 //   }
 // });
+<% } %>
